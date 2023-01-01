bdc118093e668626f7ac6521aa55ee241bba2d05
SpectreKilo <johnho8788@gmail.com>
2022-12-31 20:56:22 -0500

    started working on js file; began making variables

---
+++ assets/javascript/script.js
@@ -0,0 +1,6 @@
+var score = 0
+var highScore = localStorage.getItem("High Score");
+var initials = localStorage.getItem("Player: ");
+var finalScore = document.getElementById("finalScore");
+var playerInitials = document.getElementById("playerInitials");
+var playerScoreAndID = document.getElementById("playerScoreAndID");
