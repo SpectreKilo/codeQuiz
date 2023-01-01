318145024261a446ec58eeedc0126ff563fdd801
SpectreKilo <johnho8788@gmail.com>
2022-12-31 21:27:53 -0500

    added more variables and changed some id names to make it less confusing

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,8 +5,13 @@ var finalScore = document.getElementById("finalScore");
 var playerInitials = document.getElementById("playerInitials");
 var playerScoreAndID = document.getElementById("playerScoreAndID");
 var submitButton = document.getElementById("submitButton");
-var rightAnswer = document.getElementById("rightAnswer");
-var wrongAnswer = document.getElementById("wrongAnswer");
+var rightAnswerMessage = document.getElementById("rightAnswerMessage");
+var wrongAnswerMessage = document.getElementById("wrongAnswerMessage");
 var secondsLeft = 60;
 var remainingTime = document.getElementById("remainingTime");
 var startQuiz = document.getElementById("startQuiz");
+var startQuizButton = document.getElementById("startQuizButton");
+var questions = document.getElementById("questions");
+var questionHeader = document.getElementById("questionHeader");
+var scoreScreen = document.getElementById("scoreScreen");
+var correctAnswer
